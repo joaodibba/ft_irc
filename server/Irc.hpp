522fc0a7e97eb9871d8c1773b28d98b9e436f6bb
João Miranda <joaomiranda.pessoal@gmail.com>
2025-03-09 03:25:04 +0000

    Formating irc.hpp

--- server/Irc.hpp
+++ server/Irc.hpp
@@ -19,34 +19,34 @@
 #include <sys/epoll.h>
 #include <fcntl.h>
 
-#define END			"\033[0m"
-#define RED			"\033[1;31m"
-#define GREEN		"\033[1;32m"
-#define YELLOW		"\033[1;33m"
-#define BLUE		"\033[1;34m"
-#define MAGENTA		"\033[1;35m"
-#define CYAN		"\033[1;36m"
-#define WHITE		"\033[1;37m"
+#define END "\033[0m"
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define MAGENTA "\033[1;35m"
+#define CYAN "\033[1;36m"
+#define WHITE "\033[1;37m"
 
 #define BACKLOG 100
 #define MAX_EVENTS 1024
 
-using std::string;
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
-using std::vector;
-using std::map;
-using std::pair;
-using std::make_pair;
-using std::stringstream;
-using std::istringstream;
-using std::ofstream;
-using std::runtime_error;
-using std::ios;
+using std::exception;
 using std::find;
 using std::getline;
-using std::exception;
+using std::ios;
+using std::istringstream;
+using std::make_pair;
+using std::map;
+using std::ofstream;
+using std::pair;
+using std::runtime_error;
+using std::string;
+using std::stringstream;
+using std::vector;
 
 class Client;
 class Channel;
@@ -65,87 +65,69 @@ void logger(int type, int data);
 
 class Irc
 {
-	public:
-		Irc(void);
-		~Irc(void);
-		int run_server(char **av);
-		void setPortAndPassword(char **av);
-		void saveData(void) const;
-		// ServerUtils
-		Client *findClient(int target);
-		Client *findClient(string name);
-		Channel *findChannel(string name);
-		Channel *createChannel(string name);
-		void deleteChannel(string name);
-		void leaveAllChannels(Client *ptr);
-		void deleteClient(map<int, Client*>::iterator &it);
-		
+public:
+	Irc(void);
+	~Irc(void);
+	int run_server(char **av);
+	void setPortAndPassword(char **av);
+	void saveData(void) const;
+	// ServerUtils
+	Client *findClient(int target);
+	Client *findClient(string name);
+	Channel *findChannel(string name);
+	Channel *createChannel(string name);
+	void deleteChannel(string name);
+	void leaveAllChannels(Client *ptr);
+	void deleteClient(map<int, Client *>::iterator &it);
 
-	private:
-		int _port;
-		string _serverPassWord;
+private:
+	int _port;
+	string _serverPassWord;
 
-		int _serverSock;
-		EpollManager *epfds;
-		map<int , Client*> _clients;
-		map<int, string> requests;
-		vector<Channel*> _serverChannels;
+	int _serverSock;
+	EpollManager *epfds;
+	map<int, Client *> _clients;
+	map<int, string> requests;
+	vector<Channel *> _serverChannels;
 
-		void setPassword(string password);
-		void setPort(int port);
+	void setPassword(string password);
+	void setPort(int port);
 
-		// ServerNetwork
-		void initNetwork(void);
-		void setNonBlocking(int fd);
-		bool isNewClient(int targetFd);
-		void acceptClient(int serverFd);
+	// ServerNetwork
+	void initNetwork(void);
+	void setNonBlocking(int fd);
+	bool isNewClient(int targetFd);
+	void acceptClient(int serverFd);
 
-		// Server
-		void sendResponse(int targetFd);
-		void receiveRequest(int targetFd);
+	// Server
+	void sendResponse(int targetFd);
+	void receiveRequest(int targetFd);
 
-		// ServerChannelModes
-		void applyInviteOnlyFlag(bool optr, Channel *targetChannel);
-		void applyTopicRestrictionFlag(bool optr, Channel *targetChannel);
-		void applyMode(istringstream &ss, Channel *targetChannel, Client *client, string modeFlag); // ! FIXME maybe use this 
-		bool applyPasswordFlag(istringstream &ss, string &modeFlag, Client *client, Channel *targetChannel);
-		bool applyLimitRestrictionFlag(istringstream &ss, string &modeFlag, Client *client, Channel *targetChannel);
-		bool applyOperatorPrivilegeFlag(istringstream &ss, string &modeFlag, Client *client, Channel *targetChannel);
+	// ServerChannelModes
+	void applyInviteOnlyFlag(bool optr, Channel *targetChannel);
+	void applyTopicRestrictionFlag(bool optr, Channel *targetChannel);
+	void applyMode(istringstream &ss, Channel *targetChannel, Client *client, string modeFlag); // ! FIXME maybe use this
+	bool applyPasswordFlag(istringstream &ss, string &modeFlag, Client *client, Channel *targetChannel);
+	bool applyLimitRestrictionFlag(istringstream &ss, string &modeFlag, Client *client, Channel *targetChannel);
+	bool applyOperatorPrivilegeFlag(istringstream &ss, string &modeFlag, Client *client, Channel *targetChannel);
 
-		//Commands
-		typedef void (Irc::*CommandPtr)(istringstream &line, Client *client);
-		map<string, CommandPtr> cmds;
+	//Commands
+	typedef void (Irc::*CommandPtr)(istringstream &line, Client *client);
+	map<string, CommandPtr> cmds;
 
-		void sendToChannel(Client* sender, const string& channelName, const string& message);
-		void sendToUser(Client* sender, const string& recipient, const string& message);
+	void sendToChannel(Client *sender, const string &channelName, const string &message);
+	void sendToUser(Client *sender, const string &recipient, const string &message);
 
-		void privmsgCmd(istringstream &ss, Client *client);
-		void joinCmd(istringstream &ss, Client *client);
-		void partCmd(istringstream &ss, Client *client);
-		void topicCmd(istringstream &ss, Client *client);
-		void modeCmd(istringstream &ss, Client *client);
-		void passCmd(istringstream &ss, Client *client);
-		void nickCmd(istringstream &ss, Client *client);
-		void userCmd(istringstream &ss, Client *client);
-		void inviteCmd(istringstream &ss, Client *client);
-		void quitCmd(istringstream &ss, Client *client);
-		void kickCmd(istringstream &ss, Client *client);
-		void infoCmd(istringstream &ss, Client *client);
-	
-	public:
-		Irc(void);
-		~Irc(void);
-		int run_server(char **av);
-		void setPortAndPassword(char **av);
-
-		// ServerUtils
-		Client *findClient(int target);
-		Client *findClient(string name);
-		Channel *findChannel(string name);
-		Channel *createChannel(string name);
-		void deleteChannel(string name);
-		void leaveAllChannels(Client *ptr);
-		void deleteClient(map<int, Client*>::iterator &it);
-
-		void saveData(void) const;
+	void privmsgCmd(istringstream &ss, Client *client);
+	void joinCmd(istringstream &ss, Client *client);
+	void partCmd(istringstream &ss, Client *client);
+	void topicCmd(istringstream &ss, Client *client);
+	void modeCmd(istringstream &ss, Client *client);
+	void passCmd(istringstream &ss, Client *client);
+	void nickCmd(istringstream &ss, Client *client);
+	void userCmd(istringstream &ss, Client *client);
+	void inviteCmd(istringstream &ss, Client *client);
+	void quitCmd(istringstream &ss, Client *client);
+	void kickCmd(istringstream &ss, Client *client);
+	void infoCmd(istringstream &ss, Client *client);
 };
