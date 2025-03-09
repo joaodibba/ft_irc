e429a2fbc3fa45bcd56857bdbf25282da06a6eb0
João Miranda <joaomiranda.pessoal@gmail.com>
2025-03-09 03:14:08 +0000

    Refactor Epoll Manager

--- epoll/EpollManager.cpp
+++ epoll/EpollManager.cpp
@@ -1,47 +1,126 @@
 #include "EpollManager.hpp"
 
-EpollManager::EpollManager(void):epSock(epoll_create(1)) {
-	if (epSock < 0)	
-		throw runtime_error("creating epoll instance");
+/**
+ * @brief Construct a new Epoll Manager object
+ *
+ * Creates a new epoll instance and stores the file descriptor in epSock member.
+ * 
+ * @throws std::runtime_error if epoll_create fails
+*/
+EpollManager::EpollManager(void) : epSock(epoll_create(1))
+{
+	if (epSock < 0)
+		throw runtime_error("creating epoll instance: " + std::string(strerror(errno)));
 }
 
-EpollManager::~EpollManager(void){
-	for (size_t i = 0; i < listFds.size(); i++)
+/**
+ * @brief Destroy the Epoll Manager object
+ *
+ * Closes the epoll instance and deletes all file descriptors stored in listFds. 
+*/
+
+EpollManager::~EpollManager(void)
+{
+	for (size_t i = 0; i < listFds.size(); ++i)
+	{
 		deleteFd(listFds[i]);
+	}
 	close(epSock);
 }
 
-void EpollManager::addFd(int targetFd, uint32_t newEvent) {
-	struct epoll_event ev;
+/**
+ * @brief Get the epoll file descriptor
+ * 
+ * @return int epoll file descriptor
+ */
+int EpollManager::getEpSock(void) const
+{
+	return epSock;
+}
 
-	bzero(&ev, sizeof(ev));
-	ev.events = newEvent;
-	ev.data.fd = targetFd;
-	if (epoll_ctl(epSock, EPOLL_CTL_ADD, targetFd, &ev) == -1)
-		throw runtime_error("Cannot add server sock in the epoll instance");
+/**
+ * @brief Add a file descriptor to the epoll instance
+ * 
+ * Adds a file descriptor to the epoll instance and stores it in listFds.
+ * 
+ * @param targetFd file descriptor to add
+ * @param newEvent event to listen
+*/
+void EpollManager::addFd(int targetFd, uint32_t newEvent)
+{
+	modifyEpollFd(EPOLL_CTL_ADD, targetFd, newEvent);
 	listFds.push_back(targetFd);
 }
 
-void EpollManager::modFd(int targetFd, uint32_t newEvent) {
-	struct epoll_event ev;
+/**
+ * @brief Modify a file descriptor in the epoll instance
+ * 
+ * Modifies a file descriptor in the epoll instance.
+ * If the file descriptor is not found in listFds, it does nothing.
+ * 
+ * @param targetFd file descriptor to modify
+ * @param newEvent new event to listen
+*/
+void EpollManager::modFd(int targetFd, uint32_t newEvent)
+{
+	bool found = false;
 
-	if (find(listFds.begin(), listFds.end(), targetFd) == listFds.end())
+	for (size_t i = 0; i < listFds.size(); ++i)
+	{
+		if (listFds[i] == targetFd)
+		{
+			found = true;
+			break;
+		}
+	}
+
+	if (!found)
 		return;
-	bzero(&ev, sizeof(ev));
-	ev.events = newEvent;
-	ev.data.fd = targetFd;
-	if (epoll_ctl(epSock, EPOLL_CTL_MOD, targetFd, &ev) == -1)
-		throw runtime_error("Cannot modify the fd in poll instance");
+
+	modifyEpollFd(EPOLL_CTL_MOD, targetFd, newEvent);
 }
 
-void EpollManager::deleteFd(int targetFd){
+/**
+ * @brief Delete a file descriptor from the epoll instance
+ * 
+ * Deletes a file descriptor from the epoll instance and closes the file descriptor.
+ * If the file descriptor is not found in listFds, it does nothing.
+ * 
+ * @param targetFd file descriptor to delete
+ */
+void EpollManager::deleteFd(int targetFd)
+{
 	epoll_ctl(epSock, EPOLL_CTL_DEL, targetFd, NULL);
 	close(targetFd);
-	vector<int>::iterator it;
-	it = find(listFds.begin(), listFds.end(), targetFd);
-	listFds.erase(it);
+
+	for (size_t i = 0; i < listFds.size(); ++i)
+	{
+		if (listFds[i] == targetFd)
+		{
+			listFds.erase(listFds.begin() + i);
+			break;
+		}
+	}
 }
 
-int EpollManager::getEpSock(void) const {
-	return (epSock);
+/**
+ * @brief Modify a file descriptor in the epoll instance
+ * 
+ * Modifies a file descriptor in the epoll instance.
+ * 
+ * @param op operation to perform (EPOLL_CTL_ADD, EPOLL_CTL_MOD, EPOLL_CTL_DEL)
+ * @param targetFd file descriptor to modify
+ * @param newEvent new event to listen
+ * 
+ * @throws std::runtime_error if epoll_ctl fails
+ */
+void EpollManager::modifyEpollFd(int op, int targetFd, uint32_t newEvent)
+{
+	struct epoll_event ev;
+	memset(&ev, 0, sizeof(ev));
+	ev.events = newEvent;
+	ev.data.fd = targetFd;
+
+	if (epoll_ctl(epSock, op, targetFd, &ev) == -1)
+		throw runtime_error("Cannot modify fd in epoll instance: " + std::string(strerror(errno)));
 }
