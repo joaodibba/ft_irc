204f6131b0b8b1b1df6fd284abcf160964f43f15
João Miranda <joaomiranda.pessoal@gmail.com>
2025-03-13 18:47:11 +0000

    Add Documentatio for PASS command

--- cmd/passCmd.cpp
+++ cmd/passCmd.cpp
@@ -1,6 +1,27 @@
 #include "../server/Irc.hpp"
 
-// TODO Ensure this command follows the RFC https://www.rfc-editor.org/rfc/rfc2812.html#section-3.1.1
+/**
+ * @brief used to set the password for the client
+ * 
+ * Syntax:
+ * - PASS <password>
+ * 
+ * Authenticate the client with the server. The client must provide the correct password
+ * to proceed with the registration process. If the password is incorrect, the client is
+ * disconnected from the server.
+ * 
+ * @param ss input string stream with the password
+ * @param client the client trying to authenticate
+ * 
+ * Numeric Replies:
+ * - ERR_NEEDMOREPARAMS (461) - Missing parameters.
+ * - ERR_ALREADYREGISTRED (462) - The client is already registered.
+ * - ERR_PASSWDMISMATCH (464) - The provided password is incorrect.
+ * 
+ * @see https://www.rfc-editor.org/rfc/rfc2812.html#section-3.1.1
+ *  
+*/
+
 void Irc::passCmd(istringstream &ss, Client *client)
 {
 	string str;
