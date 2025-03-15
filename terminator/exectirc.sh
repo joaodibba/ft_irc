9848b4f4dfda63cc5a68101e32d1219b6076d8f3
ronaldprme <ronaldpr@me.com>
2025-03-15 11:59:54 +0000

    Adjustment  terminator

--- terminator/exectirc.sh
+++ terminator/exectirc.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-<<<<<<< HEAD
 # Obter o caminho do diretório onde o script está localizado
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Caminho do arquivo atual
 CURRENT_FILE="$SCRIPT_DIR/irc_terminator3"
+CURRENT_FILE="$SCRIPT_DIR/irc_terminator3"
 
 # Diretório de destino
+DEST_DIR="$HOME/.config/terminator"
 DEST_DIR="$HOME/.config/terminator"
 
 # Nome do link simbólico
@@ -20,5 +21,7 @@ ln -sf "$CURRENT_FILE" "$DEST_DIR/$LINK_NAME"
 
 echo "Link simbólico criado: $DEST_DIR/$LINK_NAME -> $CURRENT_FILE"
 
+# # Executa o arquivo
+# bash "$CURRENT_FILE"
 # # Executa o arquivo
 # bash "$CURRENT_FILE"
