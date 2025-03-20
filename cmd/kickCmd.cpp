6dfeae9d556a53f2656e89f8c7daa570bd7a8f63
João Miranda <joaomiranda.pessoal@gmail.com>
2025-03-20 15:23:51 +0000

    Enhance KICK command documentation and add leave channel

--- cmd/kickCmd.cpp
+++ cmd/kickCmd.cpp
@@ -1,9 +1,28 @@
 #include "../server/Irc.hpp"
 
-// TODO Ensure this command follows the RFC https://www.rfc-editor.org/rfc/rfc2812.html#section-3.2.8
+/**
+ * @brief Handles the KICK command for removing a user from a channel.
+ *
+ * Syntax:
+ * - KICK <channel> <nick> [<message>]
+ *   (Note: This implementation currently does not support passing multiple channels and nicks in a single command)
+ * 
+ * @param ss Input string stream containing the command arguments.
+ * @param client The client issuing the JOIN command.
+ *
+ * Numeric Replies:
+ * - ERR_NEEDMOREPARAMS (461) - Not enough parameters.
+ * - ERR_NOSUCHCHANNEL (403) - The channel does not exist.
+ * - ERR_CHANOPRIVSNEEDED (482) - The client does not have operator privileges on the channel.
+ * - ERR_USERNOTINCHANNEL (441) - The target is not on that channel.
+ * - ERR_NOTONCHANNEL (442) - The client is not on that channel.
+ * - ERR_NOSUCHNICK (401) - The target does not exist.
+ * 
+ * @see  https://www.rfc-editor.org/rfc/rfc2812.html#section-3.2.8
+ * 
+ */
 void Irc::kickCmd(istringstream &ss, Client *client)
 {
-
 	string channelName;
 	string nickName;
 	string reason;
@@ -28,12 +47,13 @@ void Irc::kickCmd(istringstream &ss, Client *client)
 	if (!channel->is_member(targetClient))
 		return sendMsg(client->getSock(), ERR_USERNOTINCHANNEL(client->getNick(), targetClient->getNick(), channelName));
 
+	// get the rest of the message
 	string tmp;
 	getline(ss, tmp);
 	reason += tmp;
-	cout << "Send to client fd: " << client->getUser() << endl;
+
 	channel->send_message(RPL(client->getNick(), client->getUser(), "KICK", (channelName + " " + targetClient->getNick()), " ", reason));
-	channel->remove_client(targetClient);
+	channel->leave_channel(targetClient);
 	if (channel->size() == 0)
 	{
 		_serverChannels.erase(find(_serverChannels.begin(), _serverChannels.end(), channel));
